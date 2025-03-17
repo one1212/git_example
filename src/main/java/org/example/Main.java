492542527cb764b2c23cbe43e75eaf2bc6f2fb61
one1212 <gksthf6424@naver.com>
2025-03-17 21:18:55 +0900

    깃 세팅해보기

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        // System.out 은 표준이라는 의미
+//        //println()은 괄호 안에 데이터를 출력하겠다
+//        // ; 반점은 코드의 마침표
+//        System.out.printf("Hello and welcome!");
+//
+//        //ln은 자동 줄바꿈이다
+//        System.out.println("123");
+//        System.out.println(123);
+//        System.out.println("hello");
+//        //sout + tab는 System.out.println(): 자동완성이다
+//        System.out.println();
+//
+//    }
+//}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        // System.out.println();
+    }
+}
